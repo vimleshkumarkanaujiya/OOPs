e871e7efb6fdc1fe0aebaa9bfbb86fa2917bc855
Vimlesh Kumar Kanaujiya <kanaujiyavimleshkumar@gmail.com>
2023-07-29 03:45:15 +0530

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -1,17 +1,21 @@
+#include "class.h"
+
 #include <iostream>
+
 using namespace std;
 
-class arya {
-  int i = 0;
- public :
-  void setValue(const int &value){ i = value;}
-  int getValue() const {return i;}
-};
+void arya::setValue(const int & value) {
+   i = value;
+}
 
-int main(){
-	arya a;
-	const int i = 1;
-	a.setValue(i);
-	cout << a.getValue();
-	return 0;
+int arya::getValue() const {
+   return i;
+}
+
+int main() {
+   arya a;
+   const int i = 1;
+   a.setValue(i);
+   cout << a.getValue();
+   return 0;
 }
