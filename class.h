ce05b21ec2084a4dd618efb6bd07541d06500e5a
Vimlesh Kumar Kanaujiya <kanaujiyavimleshkumar@gmail.com>
2023-07-29 03:46:12 +0530

    Create class.h

---
+++ class.h
@@ -0,0 +1,14 @@
+// class.h
+
+#ifndef CLASS_H // Header guard to prevent multiple inclusion
+#define CLASS_H
+
+// Function prototypes
+class arya {
+   int i = 0;
+   public:
+      void setValue(const int & );
+   int getValue() const;
+};
+
+#endif // CLASS_H
